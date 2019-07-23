8c7e8d942d0bf7e5a77531594a1d5de3903e4361
Antonio Tenorio <ctenorio@estrasol.com.mx>
2019-07-23 14:42:03 -0500

    Creacion de modal error

---
+++ resources/views/admin/elements/error-modal.blade.php
@@ -0,0 +1,19 @@
+<!-- Danger Alert Modal -->
+<div id="modal-error" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content modal-filled bg-danger">
+            <div class="modal-body p-4">
+                <div class="text-center">
+                    <i class="dripicons-wrong h1"></i>
+                    <h4 class="mt-2">Error!</h4>
+                    <p class="mt-3">
+                        @foreach ($errors->all() as $error)
+                        <span>{{ $error }}</span>
+                        @endforeach
+                    </p>
+                    <button type="button" class="btn btn-light my-2" data-dismiss="modal">Continuar</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
